02d67b1ffef6bf124f3cdabaf8e0416d934e3da1
07Kevin <07kevingustafsson@gmail.com>
2024-10-08 20:53:37 +0200

    fix

--- target/DOD/playground/PLAY_Kevin.cpp
+++ target/DOD/playground/PLAY_Kevin.cpp
@@ -7,5 +7,71 @@
 
 #include "catch2/catch_amalgamated.hpp"
 
+class Position
+{
+public:
+
+   // Constructors
+
+   Position() 
+   {
+      m_uX = 0;
+      m_uY = 0;
+   }
+
+   Position(unsigned int uX, unsigned int uY, std::string stringWord)
+   {
+      m_uX = uX;
+      m_uY = uY;
+      m_stringWord = stringWord;
+   }
+
+   //Functions
+
+   void Build()
+   {
+      const unsigned int uSize = 5;
+      std::string stringSpace[uSize][uSize];
+
+      for (int i = 0; i < uSize; i++)
+      {
+         for (int j = 0; j < uSize; j++)
+         {
+            stringSpace[i][j] = " ";
+         }
+      }
+
+      if (m_uX < uSize && m_uY < uSize)
+      {
+         stringSpace[m_uX][m_uY] = m_stringWord;
+      }
+
+      for (int i = 0; i < uSize; i++)
+      {
+         for (int j = 0; j < uSize; j++)
+         {
+            std::cout << stringSpace[i][j];
+         }
+
+         std::cout << std::endl;
+      }
+
+   }
+
+   // Member variables
+
+   std::string m_stringWord;
+   unsigned int m_uX;
+   unsigned int m_uY;
+};
+
 TEST_CASE( "[kevin] 01", "[kevin]" ) {
+   
+   Position position(2, 4, "Hello");
+
+   position.Build();
+   
+   std::cout << "Hello";
+
+   std::cout << std::endl;
 }
