3ad9819abf0e8dd409e9fb53f4e65149dfa90520
Per Ghosh <per@gorep.se>
2024-11-10 15:28:47 +0100

    feat: insert named value into shared arguments

--- target/DOD/playground/PLAY_gd.cpp
+++ target/DOD/playground/PLAY_gd.cpp
@@ -211,6 +211,7 @@ TEST_CASE( "[gd] arguments shared", "[gd]" ) {
       gd::argument::shared::arguments arguments_;
       arguments_.append_many( 100, 200, 300, 400, 500 );
       arguments_.insert( 2, "test", 250, gd::argument::shared::arguments::tag_view{});
+      std::cout << arguments_.print() << "\n";
    }
 
 
