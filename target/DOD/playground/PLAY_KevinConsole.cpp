26d762ed741f9372eeec0f124e47e39b08919f0a
07Kevin <07kevingustafsson@gmail.com>
2024-11-17 19:02:43 +0100

    fix

--- target/DOD/playground/PLAY_KevinConsole.cpp
+++ target/DOD/playground/PLAY_KevinConsole.cpp
@@ -2,11 +2,14 @@
 #include "gd/gd_arguments.h"
 #include "gd/gd_variant.h"
 #include "gd/gd_table_column-buffer.h"
+#include "gd/gd_console_print.h"
+#include "gd/gd_console_style.h"
 
 #include <chrono>
 #include <thread>
 
 #include "main.h"
+
 
 #include "catch2/catch_amalgamated.hpp"
 
@@ -15,62 +18,108 @@ void MoveCursor(int iRow, int iColumn)
    std::cout << "\033[" << iRow << ";" << iColumn << "H";
 }
 
-class Device
+struct box
+{
+public:
+   
+   box(unsigned int uRow, unsigned int uColumn): m_uRow(uRow), m_uColumn(uColumn) {}
+
+   box& move_up() { m_uRow--; return *this; }
+   box& move_up(unsigned uMove) { m_uRow -= uMove; return *this; }
+   box& move_down() { m_uRow++; return *this; }
+   box& move_down(unsigned uMove) { m_uRow += uMove; return *this; }
+   
+   box& move_left() { m_uColumn--; return *this; }
+   box& move_left(unsigned uMove) { m_uColumn -= uMove; return *this; }
+   box& move_right() { m_uColumn++; return *this; }
+   box& move_right(unsigned uMove) { m_uColumn += uMove; return *this; }
+
+   void print(gd::console::device* pdevice, unsigned int uLength, unsigned int uWidth, char iCharacter)
+   {
+      unsigned uR = m_uRow, uC = m_uColumn;
+
+      for (unsigned int ui = 0; ui < uLength; ui++)
+      {
+         for (unsigned int uj = 0; uj < uWidth; uj++)
+         {
+            pdevice->print(ui + uR, uj + uC, iCharacter);
+         }
+      }
+   }
+
+   char m_iCharacter = 0;
+   unsigned int m_uRow;
+   unsigned int m_uColumn;
+};
+
+struct triangle
 {
 public:
 
-   Device()
-   {
-      m_uWidth = 0;
-      m_uRow = 0;
-   }
-   
-   Device(unsigned int uWidth, unsigned int uRow)
-   {
-      m_uWidth = uWidth;
-      m_uRow = uRow;
+   triangle(unsigned int uRow, unsigned int uColumn) : m_uRow(uRow), m_uColumn(uColumn) {}
 
-      m_vectorDevice.resize(m_uWidth * m_uRow);
-   }
+   triangle& move_up() { m_uRow--; return *this; }
+   triangle& move_up(unsigned uMove) { m_uRow -= uMove; return *this; }
+   triangle& move_down() { m_uRow++; return *this; }
+   triangle& move_down(unsigned uMove) { m_uRow += uMove; return *this; }
 
-   void Fill(char cCharacter)
+   triangle& move_left() { m_uColumn--; return *this; }
+   triangle& move_left(unsigned uMove) { m_uColumn -= uMove; return *this; }
+   triangle& move_right() { m_uColumn++; return *this; }
+   triangle& move_right(unsigned uMove) { m_uColumn += uMove; return *this; }
+
+   void print(gd::console::device* pdevice, unsigned int uSize, char iCharacter)
    {
-      for (int i = 0; i < m_vectorDevice.size(); i++)
+      unsigned uR = m_uRow, uC = m_uColumn;
+
+      for (int i = 0; i < uSize; i++)
       {
-         m_vectorDevice[i] = cCharacter;
-      }
-   }
-
-   std::string ToString()
-   {
-      std::string stringDevice;
-
-      for (int iRow = 0; iRow < m_uRow; iRow++)
-      {
-         for (int iColumn = 0; iColumn < m_uWidth; iColumn++)
+         for (int j = 0; j < i + 1; j++)
          {
-            char cFetch = m_vectorDevice[iRow * m_uWidth + iColumn];
-            stringDevice += cFetch;
+            pdevice->print(i + uR, j + uC, iCharacter);
          }
-         stringDevice += "\n";
       }
-      return stringDevice;
+
    }
 
-   unsigned int m_uWidth;
+   char m_iCharacter = 0;
    unsigned int m_uRow;
-
-   std::vector<char> m_vectorDevice;
+   unsigned int m_uColumn;
 };
-
 
 TEST_CASE(" [kevin] 02", "[kevin] ")
 {
-   Device device(20, 10);
+   gd::console::device deviceTest(250, 100);
+   deviceTest.create();
 
-   device.Fill('U');
+   box boxTest(1, 1);
 
-   std::cout << device.ToString() << std::endl;
+   triangle triangleTest(1, 1);
+
+   deviceTest.create(20, 100);
+
+   auto stringOut = deviceTest.render(gd::console::tag_format_cli{});
+   std::cout << stringOut;
+   
+   boxTest.print(&deviceTest, 5, 10, '*');
+
+   boxTest.move_right(10);
+
+   boxTest.print(&deviceTest, 5, 15, '-');
+
+   boxTest.move_down(5);
+   boxTest.move_left(10);
+
+   boxTest.print(&deviceTest, 5, 25, '-');
+
+
+   triangleTest.move_right(30);
+   triangleTest.print(&deviceTest, 10, '#');
+
+   //std::cout << " ";
+
+   stringOut = deviceTest.render(gd::console::tag_format_cli{});
+   std::cout << stringOut;
 
    std::cout << "Hello\n";
 
