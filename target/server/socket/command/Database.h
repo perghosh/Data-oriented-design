75ddd238114b9d56add311b37c409544a1adf76b
Per Ghosh <per@gorep.se>
2025-01-25 01:06:03 +0100

    chore

---
+++ target/server/socket/command/Database.h
@@ -0,0 +1,16 @@
+/**
+* \file command/Database.h
+* 
+* \brief Handle database actions
+* Metods to handle database actions like open, close, execute and ask databases for information.
+* 
+*/
+
+#pragma once
+
+#include <cassert>
+#include <functional>
+#include <string>
+#include <string_view>
+
+#include "Router.h"
