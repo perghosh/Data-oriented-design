f17aba5acf3c8abac0f7c9dff1ec66f605e0125a
Per Ghosh <per@gorep.se>
2025-03-20 07:00:17 +0100

    docs

--- target/TOOLS/FileCleaner/Document.cpp
+++ target/TOOLS/FileCleaner/Document.cpp
@@ -15,6 +15,17 @@ void CDocument::common_construct(CDocument&& o) noexcept
    m_vectorData = std::move(o.m_vectorData);
 }
 
+/** ---------------------------------------------------------------------------
+ * @brief Loads a file into the document from the specified path.
+ *
+ * This method attempts to open and read the contents of a file specified by the given path into the document's internal data structure (`m_vectorData`).
+ * The file is read in binary mode, and its size is determined to pre-allocate memory for efficiency. If successful, the path is stored in the document.
+ *
+ * @param stringPath A string view representing the path to the file to be loaded.
+ * @return A pair containing:
+ *         - `bool`: `true` if the file was successfully loaded, `false` otherwise.
+ *         - `std::string`: An empty string on success, or an error message on failure (e.g., "Failed to open file: <path>").
+ */
 std::pair<bool, std::string> CDocument::Load(const std::string_view& stringPath)
 {
    std::ifstream ifstreamFile(stringPath.data(), std::ios::binary | std::ios::ate);
@@ -35,6 +46,17 @@ std::pair<bool, std::string> CDocument::Load(const std::string_view& stringPath)
 }
 
 
+/** ---------------------------------------------------------------------------
+ * @brief Saves the document's data to a file at the specified path.
+ *
+ * This method writes the contents of the document's internal data structure (`m_vectorData`) to a file in binary mode.
+ * If the file cannot be opened or written to, an error message is returned.
+ *
+ * @param stringPath A string view representing the path where the file should be saved.
+ * @return A pair containing:
+ *         - `bool`: `true` if the file was successfully saved, `false` otherwise.
+ *         - `std::string`: An empty string on success, or an error message on failure (e.g., "Failed to write to file: <path>").
+ */
 std::pair<bool, std::string> CDocument::Save(const std::string_view& stringPath)
 {
    std::ofstream ofstreamFile(stringPath.data(), std::ios::binary);
@@ -46,6 +68,14 @@ std::pair<bool, std::string> CDocument::Save(const std::string_view& stringPath)
    return {true, ""};
 }
 
+/** ---------------------------------------------------------------------------
+ * @brief Counts the occurrences of a specific character in the document's data.
+ *
+ * This method scans the document's internal data (`m_vectorData`) and returns the number of times a given character appears.
+ *
+ * @param uCharacter The 8-bit unsigned integer representing the character to count.
+ * @return The number of occurrences of the specified character in the document's data.
+ */
 size_t CDocument::Count(uint8_t uCharacter) const
 {
    auto uCount = std::count(m_vectorData.begin(), m_vectorData.end(), uCharacter);
