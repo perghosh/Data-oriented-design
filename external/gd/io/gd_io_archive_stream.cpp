40953025870a51441093abb5399bf3462b7bbd0c
Per Ghosh <per@gorep.se>
2025-03-22 04:02:31 +0100

    feat: added support for strings in archive

--- external/gd/io/gd_io_archive_stream.cpp
+++ external/gd/io/gd_io_archive_stream.cpp
@@ -3,6 +3,38 @@
 
 _GD_IO_STREAM_BEGIN
 
+
+archive& archive::read( std::string& string_ )
+{
+   uint64_t uSize;
+   read_size(uSize);
+   string_.resize(uSize);
+   read(string_.data(), uSize);
+   return *this;
+}
+
+archive& archive::write( const std::string_view& stringData )
+{
+   write_size((uint64_t)stringData.size());
+   write(stringData.data(), stringData.size());
+   return *this;
+}
+
+archive& archive::write( const std::string& stringData )
+{
+   write_size((uint64_t)stringData.size());
+   write(stringData.data(), stringData.size());
+   return *this;
+}
+
+
+/** ---------------------------------------------------------------------------
+ * @brief open file stream
+ * @param fstreamFile file stream object to open
+ * @param stringPath path to file
+ * @param mode_ in which mode to open file
+ * @return true if file is opened, false if failed to open file
+ */
 std::pair<bool, std::string> archive::open_s(std::fstream& fstreamFile, const std::string_view& stringPath, std::ios_base::openmode mode_) 
 {
    fstreamFile.open( stringPath.data(), mode_ );
