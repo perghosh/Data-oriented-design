ae1a332def187aa01e640d08558211683cd7ea73
Per <per@gorep.se>
2025-03-28 22:29:39 +0100

    fix

---
+++ target/TOOLS/FileCleaner/tests/History.cpp
@@ -0,0 +1,43 @@
+#include "History.h"
+
+void CHistory::Add(const std::string& stringFile, const std::string& stringDescription)
+{
+   std::filesystem::path pathFile(stringFile);
+
+   if( std::filesystem::exists(pathFile) )
+   {
+      m_vectorList.emplace_back(stringFile, stringDescription);
+   }
+   else
+   {
+      return;
+   }
+
+}
+
+void CHistory::Write(gd::io::stream::archive& archive_)
+{
+
+   size_t iCount = m_vectorList.size();
+   archive_ << iCount;
+
+   for( int i = 0; i < iCount; i++ )
+   {
+      archive_ << std::get<0>(m_vectorList[i]) << std::get<1>(m_vectorList[i]);
+   }
+}
+
+void CHistory::Read(gd::io::stream::archive& archive_)
+{
+   m_vectorList.clear();
+
+   size_t iCount = 0;
+   archive_ >> iCount;
+
+   for( size_t i = 0; i < iCount; i++ )
+   {
+      std::string stringTemp, stringTemp2;
+      archive_ >> stringTemp >> stringTemp2;
+      m_vectorList.emplace_back(stringTemp, stringTemp2);
+   }
+}
