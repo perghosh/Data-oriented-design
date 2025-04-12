6ddde4466db15eab6c5292830f417a50675b1ef6
per <per@gorep.se>
2025-04-13 01:27:49 +0200

    chore: fix document on work computer

--- target/TOOLS/FileCleaner/Document.h
+++ target/TOOLS/FileCleaner/Document.h
@@ -11,6 +11,7 @@
 
 #include <memory>
 #include <mutex>
+#include <shared_mutex>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -21,9 +22,10 @@
 #include "gd/gd_uuid.h"
 #include "gd/gd_arguments.h"
 #include "gd/gd_arguments_shared.h"
+#include "gd/gd_table_column-buffer.h"
 
 
-
+class CApplication;
 
 
 /**
@@ -39,7 +41,7 @@ class CDocument
 // ## construction -------------------------------------------------------------
 public: // 0TAG0construct.Document
    CDocument() {}
-   CDocument( const std::string_view& stringName ) { m_arguments.append( "name", stringName ); }
+   CDocument( CApplication* papplication, const std::string_view& stringName ): m_papplication(papplication) { m_arguments.append( "name", stringName ); }
    CDocument( const gd::argument::shared::arguments& arguments_ ): m_arguments( arguments_ ) {}
 // copy
    CDocument(const CDocument& o) { common_construct(o); }
@@ -62,6 +64,8 @@ public:
 public:
 /** \name GET/SET
 *///@{
+   CApplication* GetApplication() { return m_papplication; }
+   const CApplication* GetApplication() const { return m_papplication; }
    gd::variant_view Get(const std::string_view& stringName) const { return m_arguments[stringName].as_variant_view(); }
    void Set(const std::string_view& stringName, const gd::variant_view& variantValue) { m_arguments.set(stringName, variantValue); }
 
@@ -70,6 +74,29 @@ public:
    std::string_view Getpath() const { return m_arguments["path"].as_string_view(); }
    void SetPath(const std::string_view& stringName) { m_arguments.set("path", stringName); }
 
+//@}
+
+/** \name CACHE
+ * Document are able to cache information and it can handle different named caches.
+ * Each cache is stored in a named table and that table is then stored in 
+ * member vector in document called `m_vectorTableCache`.
+ * Information about cached information (valid cache tables) are read from file, this file
+ * is configurable but check for `cache.xml`, it might be that cache logic is placed there.
+ *///@{
+   /// Load cache data
+   std::pair<bool, std::string> CACHE_Load( const std::string_view& stringId );
+   bool CACHE_Add( gd::table::dto::table&& table, const std::string_view& stringId );
+   bool CACHE_Add( gd::table::dto::table&& table ) { return CACHE_Add( std::forward<gd::table::dto::table>( table ), std::string_view{}); }
+   /// Return table with cache data
+   gd::table::dto::table* CACHE_Get( const std::string_view& stringId, bool bLoad );
+   gd::table::dto::table* CACHE_Get( const std::string_view& stringId ) { return CACHE_Get( stringId, true ); }
+   /// Return information to generate cache data
+   std::pair<bool, std::string> CACHE_GetInformation( const std::string_view& stringId, gd::argument::arguments& argumentsCache );
+   gd::argument::arguments CACHE_GetInformation( const std::string_view& stringId );
+   void CACHE_Erase( const std::string_view& stringId );
+#ifndef NDEBUG
+   bool CACHE_Exists_d( const std::string_view& stringId );
+#endif // !NDEBUG
 //@}
 
 /** \name OPERATION
@@ -83,6 +110,13 @@ public:
    /// Count characters in file data
    size_t Count( uint8_t uCharacter ) const;
 //@}
+
+/** \name ERROR
+*///@{
+/// Add error to internal list of errors
+   void ERROR_Add( const std::string_view& stringError );
+
+   //@}
 
 protected:
 /** \name INTERNAL
@@ -99,8 +133,22 @@ public:
 
 // ## attributes ----------------------------------------------------------------
 public:
-   gd::argument::shared::arguments m_arguments;///< document information (members)
+   CApplication* m_papplication = nullptr;      ///< pointer to application object
+
+   gd::argument::shared::arguments m_arguments; ///< document information (members)
    std::vector<uint8_t> m_vectorData;///< document data (file content)
+
+   // ## cache information is stored in dto tables (dto = data transfer object)
+   std::shared_mutex m_sharedmutexTableCache;   ///< mutex used as lock for table methods in document
+   std::string m_stringCacheConfiguration;      ///< file name for file with cache configuration information 
+   /// vector of tables used to cache data
+   std::vector< std::unique_ptr< gd::table::dto::table > > m_vectorTableCache;
+
+   // ## Error information. Document are able to collect error information that may be usefull locating problems
+   std::shared_mutex m_sharedmutexError;        ///< mutex used to manage errors in threaded environment
+   std::vector< gd::argument::arguments > m_vectorError; ///< vector storing internal errors
+
+
 
 // ## free functions ------------------------------------------------------------
 public:
