da5666289244f29a3b0e9ec47ff132892880d345
Per <per@gorep.se>
2025-04-14 20:12:17 +0200

    fix

--- target/TOOLS/FileCleaner/Command.cpp
+++ target/TOOLS/FileCleaner/Command.cpp
@@ -1,1 +1,22 @@
 #include "Command.h"
+
+int RowCount( const std::string& stringFile )
+{
+   if( std::filesystem::is_regular_file(stringFile) == true )
+   {
+      std::ifstream ifstreamFile(stringFile);
+      std::string stringText;
+      int iCount = 0;
+
+      while( std::getline(ifstreamFile, stringText) )
+      {
+         iCount++;
+      }
+
+      ifstreamFile.close();
+
+      return iCount;
+   }
+
+   return 0;
+}
