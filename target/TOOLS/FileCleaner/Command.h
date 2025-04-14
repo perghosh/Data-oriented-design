de01e54963013215cb20bbfe4f5e8cc7cf4d63d4
Per Ghosh <per@gorep.se>
2025-04-14 19:24:52 +0200

    chore

---
+++ target/TOOLS/FileCleaner/Command.h
@@ -0,0 +1,29 @@
+/**
+ * @file Command.cpp
+ * 
+ * ### 0TAG0 File navigation, mark and jump to common parts
+ * 
+ */
+
+#pragma once
+
+
+ // ## STL
+
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+ // ## GD
+
+#include "gd/gd_uuid.h"
+#include "gd/gd_arguments.h"
+#include "gd/gd_arguments_shared.h"
+#include "gd/gd_table_column-buffer.h"
+
+
+#include "Application.h"
