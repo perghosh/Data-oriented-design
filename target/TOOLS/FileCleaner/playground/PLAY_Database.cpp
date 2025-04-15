b3cebd3733624ea7bf3b6842db20485de809c866
Per Ghosh <per@gorep.se>
2025-04-15 14:59:32 +0200

    feat,docs,test

---
+++ target/TOOLS/FileCleaner/playground/PLAY_Database.cpp
@@ -0,0 +1,19 @@
+#include "gd/gd_utf8.h"
+#include "gd/gd_arguments.h"
+#include "gd/gd_arguments_shared.h"
+
+#include "gd/gd_arguments_common.h"
+
+#include "main.h"
+
+#include "catch2/catch_amalgamated.hpp"
+
+
+TEST_CASE( "[database] test database in cleaner", "[database]" ) {
+   std::string stringFolder = FOLDER_GetRoot_g( "test__" );
+   gd::argument::arguments arguments_;
+   arguments_.append("test-folder", stringFolder);
+   arguments_.append("database-file", "test.db");
+   std::cout << arguments_.print( "Folder where database are places is {} and database file is: {}\n" );
+   //std::cout << "test database in cleaner" << std::endl;
+}
