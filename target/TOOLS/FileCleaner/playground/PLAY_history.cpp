657131c357a3253a52b1be2893438348376b6e78
Per <per@gorep.se>
2025-04-21 19:31:41 +0200

    fix

---
+++ target/TOOLS/FileCleaner/playground/PLAY_history.cpp
@@ -0,0 +1,17 @@
+#include <filesystem>
+
+#include "gd/gd_file.h"
+#include "gd/gd_utf8.h"
+#include "gd/gd_arguments.h"
+#include "gd/gd_arguments_shared.h"
+#include "gd/gd_table_column-buffer.h"
+#include "gd/gd_table_io.h"
+#include "gd/gd_sql_value.h"
+
+#include "../Application.h"
+
+#include "main.h"
+
+#include "../Command.h"
+
+#include "catch2/catch_amalgamated.hpp"
