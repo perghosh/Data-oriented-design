4df3b628bd9ed742c5b650ec3659bb7616934c6a
Per Ghosh <per@gorep.se>
2025-04-27 04:20:26 +0200

    fix

--- target/TOOLS/FileCleaner/playground/PLAY_rowcounter.cpp
+++ target/TOOLS/FileCleaner/playground/PLAY_rowcounter.cpp
@@ -115,6 +115,79 @@ TEST_CASE("[rowcouner] count comments", "[rowcouner]") {
    std::cout << "Comment count: " << uCommentCount << "\n";
 }
 
+TEST_CASE("[rowcouner] count hello world", "[rowcouner]") {
+   std::string stringFile = FOLDER_GetRoot_g("temp__/hello-world.cpp");                                  REQUIRE(std::filesystem::exists(stringFile) == true);
+
+   std::ifstream file_(stringFile, std::ios::binary);                                              REQUIRE(file_.is_open() == true);
+
+
+   gd::parse::window::line windowLine_(1024, gd::types::tag_create{});         // create line buffer
+
+   gd::expression::parse::state state_;
+   state_.add(std::string_view("COMMENT"), "//", "\n");
+   state_.add(std::string_view("COMMENTBLOCK"), "/*", "*/");
+   state_.add(std::string_view("STRING"), "\"", "\"");
+
+
+   unsigned uNewLineCount = 0;
+   bool bInside = false;
+   unsigned uCommentCount = 0;
+   unsigned uCodeCount = 0; // number of characters of code in current line
+
+   auto uAvailable = windowLine_.available();
+   file_.read((char*)windowLine_.buffer(), uAvailable);
+   auto uReadSize = file_.gcount();                                            // get number of valid bytes read
+   windowLine_.update(uReadSize);                                              // Update valid size in line buffer
+   while( windowLine_.eof() == false )
+   {
+      // ## count number of comments, comments start with '//' or '/*' and end with '*/'
+      auto [first_, last_] = windowLine_.range(gd::types::tag_pair{});
+      for(auto it = first_; it < last_; it++ ) 
+      {
+         if( state_.in_state() == false )
+         {
+            // ## check if we have found state
+            if( state_[*it] != 0 && state_.exists( it ) == true )
+            {
+               state_.activate(it);                                            // activate state
+               if( uCodeCount > 0 && state_.is_multiline() == false ) { uNewLineCount++; }
+
+               continue;
+            }
+
+            std::cout << *it;
+
+            if( *it == '\n' ) { uNewLineCount++; uCodeCount = 0; }
+            else if( gd::expression::is_code_g( *it ) != 0 ) { uCodeCount++; }
+         }
+         else
+         {                                                                                         REQUIRE( state_.in_state() == true);
+            // ## check if we have found end of state
+            unsigned uLength;
+            if( state_.deactivate( it, &uLength ) == true ) 
+            {
+               if( uLength > 1 ) it++;
+               continue;
+            }
+         }
+      }
+
+      windowLine_.rotate();                                                    // rotate buffer
+
+      if( uReadSize > 0 )                                                      // was it possible to read data last read, then more data is available
+      {
+         auto uAvailable = windowLine_.available();                            // get available space in buffer to be filled
+         file_.read((char*)windowLine_.buffer(), windowLine_.available());     // read more data into available space in buffer
+         uReadSize = file_.gcount();
+         windowLine_.update(uReadSize);                                        // update valid size in line buffer
+      }
+   }
+
+   file_.close();                                                             // close file
+   std::cout << "\nCode count: " << uNewLineCount << "\n";
+
+}
+
 TEST_CASE("[rowcouner] use states", "[rowcouner]") {
    std::string stringFile = FOLDER_GetRoot_g("temp__/sqlite3.c");                                  REQUIRE(std::filesystem::exists(stringFile) == true);
 
@@ -155,11 +228,8 @@ TEST_CASE("[rowcouner] use states", "[rowcouner]") {
                continue;
             }
 
-            if( state_[*it] == 0 )
-            {
-               if( *it == '\n' ) { uNewLineCount++; uCodeCount = 0; }
-               else if( gd::expression::is_code_g( *it ) != 0 ) { uCodeCount++; }
-            }
+            if( *it == '\n' ) { uNewLineCount++; uCodeCount = 0; }
+            else if( gd::expression::is_code_g( *it ) != 0 ) { uCodeCount++; }
          }
          else
          {                                                                                         REQUIRE( state_.in_state() == true);
