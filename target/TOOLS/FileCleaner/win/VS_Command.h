d8786a3cfbe63e1921bd286a2c43e12c20bd4f9e
Per Ghosh <per@gorep.se>
2025-05-13 00:46:03 +0200

    Add compiler warning management and COM imports
    Enhance `VS_Command.h` with preprocessor directives to handle compiler-specific warnings for Clang, GCC, and MSVC. Introduce `#pragma warning(push)` and `#pragma warning(pop)` to manage warning 4278. Add two necessary COM library imports for improved functionality.

--- target/TOOLS/FileCleaner/win/VS_Command.h
+++ target/TOOLS/FileCleaner/win/VS_Command.h
@@ -7,11 +7,20 @@
 
 #pragma once
 
+#if defined( __clang__ )
+#elif defined( __GNUC__ )
+#elif defined( _MSC_VER )
+   #pragma warning(push)
+   #pragma warning(disable : 4278)
+#endif
+
+
 #include <windows.h>
 #include <string>
 #include <iostream>
 #include <comdef.h>
 #include <atlbase.h>
+
 
 #import "libid:80cc9f66-e7d8-4ddd-85b6-d9e6cd0e93e2" version("8.0") lcid("0") raw_interfaces_only named_guids
 #import "libid:1A31287A-4D7D-413e-8E32-3B374931BD89" version("8.0") lcid("0") raw_interfaces_only named_guids
@@ -91,6 +100,13 @@ public:
 
 
 } // namespace VC
+
+#if defined(__clang__)
+#elif defined(__GNUC__)
+#elif defined(_MSC_VER)
+   #pragma warning(pop)
+#endif
+
 
 #endif // _WIN32
 
