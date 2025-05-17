6bf56d8be5f975480047424a372a30eb4c48499f
07Kevin <07kevingustafsson@gmail.com>
2025-05-17 18:17:28 +0200

    fix

--- target/TOOLS/FileCleaner/cli/CLIDir.cpp
+++ target/TOOLS/FileCleaner/cli/CLIDir.cpp
@@ -30,13 +30,29 @@ std::pair<bool, std::string> Dir_g(const gd::cli::options* poptionsDir)
 
    auto ptable = std::make_unique<gd::table::dto::table>(gd::table::dto::table(0u, { {"rstring", 0, "path"} }, gd::table::tag_prepare{}));
 
+   // Add to table
    for( const auto& it : std::filesystem::directory_iterator(stringSource) )
    {
       if( it.is_regular_file() || it.is_directory() )
       {
          std::string stringFilePath = it.path().string();
-         ptable->row_add();
-         ptable->cell_set(ptable->get_row_count() - 1, "path", stringFilePath);
+
+         if( (*poptionsDir)["filter"].is_true() == true )
+         {
+            std::string stringFilter = (*poptionsDir)["filter"].as_string();
+            std::string stringExtension = it.path().extension().string();
+
+            if( stringFilter == stringExtension )
+            {
+               ptable->row_add();
+               ptable->cell_set(ptable->get_row_count() - 1, "path", stringFilePath);
+            }
+         }
+         else
+         {
+            ptable->row_add();
+            ptable->cell_set(ptable->get_row_count() - 1, "path", stringFilePath);
+         }
       }
    }
 
