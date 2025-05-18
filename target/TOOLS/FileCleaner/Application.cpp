fd43e87fb7e2b44f68e95ec4ebba78c60038eec7
Per Ghosh <per@gorep.se>
2025-05-18 03:33:16 +0200

    fix

--- target/TOOLS/FileCleaner/Application.cpp
+++ target/TOOLS/FileCleaner/Application.cpp
@@ -313,7 +313,7 @@ std::pair<bool, std::string> CApplication::Initialize( gd::cli::options& options
    }
    else if( stringCommandName == "version" )
    {
-      std::cout << "version 0.9.3" << "\n";
+      std::cout << "version 0.9.5" << "\n";
    }
    else
    {
