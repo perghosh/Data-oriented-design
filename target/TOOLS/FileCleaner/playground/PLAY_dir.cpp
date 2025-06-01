0ceb80838e015afaddbdf9847bf572018bc3cdcb
Per Ghosh <per@gorep.se>
2025-06-01 04:22:23 +0200

    Add find_first and read_first methods to parser state
    This commit introduces two new methods, `find_first` and `read_first`, in the `gd_expression_parse_state` class to enhance the functionality for retrieving rule indices and values from input text. The methods include input validation and whitespace handling. Test cases in `PLAY_dir.cpp` have been updated to use these new methods, replacing previous implementations with different comment markers. Minor adjustments to input text handling and buffer management are also included.

--- target/TOOLS/FileCleaner/playground/PLAY_dir.cpp
+++ target/TOOLS/FileCleaner/playground/PLAY_dir.cpp
@@ -25,12 +25,27 @@
 
 TEST_CASE("[file] read .gitignore", "[file]")
 {
+   gd::expression::parse::state state_;
+   state_.add(std::string_view("LINECOMMENT"), "#", "\n");
+   gd::parse::window::line lineBuffer(256, gd::types::tag_create{});          // create line buffer 64 * 64 = 4096 bytes = 64 cache lines
+
+   {
+      std::string stringTest = "  1 2 3 4 5 6 7  # Test string";
+      auto [ iRule, piPosition ] = state_.find_first(stringTest); // Find first comment in line
+      assert( iRule == -1 );
+      assert( *piPosition == '1');
+
+      auto [ iRule2, stringValue ] = state_.read_first( stringTest );
+      assert(stringValue == "1 2 3 4 5 6 7  ");
+
+      std::string stringTest1 = "# Test string";
+      auto [ iRule3, stringValue3 ] = state_.read_first( stringTest1 );
+      assert(stringValue3 == " Test string");
+   }
+
    std::string stringFileGitignore = FOLDER_GetRoot_g(".gitignore");                               REQUIRE(std::filesystem::exists(stringFileGitignore) == true);
    std::ifstream file_(stringFileGitignore, std::ios::binary);                                     REQUIRE( file_.is_open() );
 
-   gd::expression::parse::state state_;
-   state_.add(std::string_view("LINECOMMENT"), "//", "\n");
-   gd::parse::window::line lineBuffer(64, gd::types::tag_create{});          // create line buffer 64 * 64 = 4096 bytes = 64 cache lines
 
    file_.read((char*)lineBuffer.buffer(), lineBuffer.available());
    auto uReadSize = file_.gcount();                                           // get number of valid bytes read
@@ -43,7 +58,14 @@ TEST_CASE("[file] read .gitignore", "[file]")
       while( lineBuffer.getline( stringLine ) == true )
       {
          // Process the line here, e.g., print it or store it
-         std::cout << stringLine << std::endl;
+         
+         auto [ iRule, piPosition ] = state_.find_first(stringLine); // Find first comment in line
+
+         if( iRule == -1 && piPosition != nullptr )
+         {
+            auto [ iRule, stringValue ] = state_.read_first( stringLine );
+            std::cout << stringValue << std::endl;
+         }
       }
 
       lineBuffer.rotate();
