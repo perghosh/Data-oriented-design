0aadc4f5f2a59926cc84695ad1c4f6ef5f5eeea8
Per Ghosh <per@gorep.se>
2025-06-03 10:07:08 +0200

    fix

--- target/TOOLS/FileCleaner/cli/CLIList.cpp
+++ target/TOOLS/FileCleaner/cli/CLIList.cpp
@@ -71,7 +71,7 @@ std::pair<bool, std::string> ListPattern_g(const gd::cli::options* poptionsList,
    if (iRecursive == 0 && options_.exists("R") == true) iRecursive = 16; // set to 16 if R is set, find all files
 
    std::string stringFilter = options_["filter"].as_string();
-   if( stringFilter == "*" ) 
+   if( stringFilter == "*" || stringFilter == "." || stringFilter == "**" ) 
    { 
       stringFilter.clear();                                                   // if filter is set to * then clear it, we want all files
       if( iRecursive == 0 ) iRecursive = 16;                                  // if recursive is not set, set it to 16, find all files
