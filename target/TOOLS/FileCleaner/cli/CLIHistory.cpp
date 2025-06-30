e84d18683a19ae7d7483efccfb16d78a5827198c
Per <per@gorep.se>
2025-06-30 15:53:08 +0200

    fix

--- target/TOOLS/FileCleaner/cli/CLIHistory.cpp
+++ target/TOOLS/FileCleaner/cli/CLIHistory.cpp
@@ -20,6 +20,13 @@ NAMESPACE_CLI_BEGIN
 
 std::pair<bool, std::string> History_g(const gd::cli::options* poptionsHistory)
 {
+   const gd::cli::options& options_ = *poptionsHistory;
+   if( options_.exists("create") == true )
+   {
+      gd::argument::arguments argumentsCreate( {"create", options_["create"].as_string()} );
+      auto result_ = HistoryCreate_g(argumentsCreate);
+   }
+
    return { true, "" };
 }
 
@@ -29,7 +36,32 @@ std::pair<bool, std::string> History_g(const gd::cli::options* poptionsHistory)
  */
 std::pair<bool, std::string> HistoryCreate_g( const gd::argument::arguments& argumentsCreate )
 {
-   auto result_ = papplication_g->CreateDirectory();                                               if( result_.first == false ) { return result_; }
+   //auto result_ = papplication_g->CreateDirectory();                                               if( result_.first == false ) { return result_; }
+
+   std::filesystem::path pathCurrentDirectory = std::filesystem::current_path() / ".cleaner";
+
+   if( std::filesystem::exists(pathCurrentDirectory) == false )
+   {
+      std::filesystem::create_directory(pathCurrentDirectory); 
+      std::ofstream historyFile(pathCurrentDirectory / "history.xml");
+      historyFile.close();
+   }
+   else
+   {
+      HistoryDelete_g(argumentsCreate);
+   }
+
+   return { true, "" };
+}
+
+std::pair<bool, std::string> HistoryDelete_g(const gd::argument::arguments& argumentsCreate)
+{
+   std::filesystem::path pathCurrentDirectory = std::filesystem::current_path() / ".cleaner";
+
+   if( std::filesystem::exists(pathCurrentDirectory) == true  )
+   {
+      std::filesystem::remove_all(pathCurrentDirectory); // remove the history folder
+   }
 
    return { true, "" };
 }
