d6b2ad5c2635313d11984ce9d0cf75deb54391e7
Per Ghosh <per@gorep.se>
2025-07-21 02:08:44 +0200

    fix

--- target/TOOLS/FileCleaner/playground/PLAY_strstr.cpp
+++ target/TOOLS/FileCleaner/playground/PLAY_strstr.cpp
@@ -29,7 +29,10 @@ struct code
    code(uint8_t uOpenBracket, uint8_t uCloseBracket) : m_uOpenBracket(uOpenBracket), m_uCloseBracket(uCloseBracket), m_uKeySeparator(':'), m_stringQuoteChars("\"'`"), m_uOptions(eTrimWhitespace | eAllowUnquoted) {}
    code(uint8_t uOpenBracket, uint8_t uCloseBracket, uint8_t uKeySeparator) : m_uOpenBracket(uOpenBracket), m_uCloseBracket(uCloseBracket), m_uKeySeparator(uKeySeparator), m_stringQuoteChars("\"'`"), m_uOptions(eTrimWhitespace | eAllowUnquoted) {}
 
-   bool is_quote(uint8_t uChar) const { return m_uQuotes.find(uChar) != std::string::npos; }
+   bool is_quote(uint8_t uChar) const { return m_stringQuoteChars.find(uChar) != std::string::npos; }
+
+   bool is_open_scope(uint8_t uChar) const { return m_uOpenBracket == uChar; }
+   bool is_close_scope(uint8_t uChar) const { return m_uCloseBracket == uChar; }
 
    /// Skip quoted section, returning pointer past closing quote
    const uint8_t* skip_quoted(const uint8_t* puPosition, const uint8_t* puEnd) const { assert( is_quote(*puPosition) == true );
@@ -37,27 +40,49 @@ struct code
       const uint8_t uQuote = *puPosition;
       ++puPosition; // Skip opening quote
 
-      while( *puPosition == uQuote && puPosition < puEnd ) puPosition++;
+      while( *puPosition == uQuote && puPosition < puEnd ) { puPosition++, uQuoteCount++; }
 
-      uQuoteCount
+      if( uQuoteCount % 2 == 0 ) return (puPosition + 1);                      // Even number of quotes, return position after closing quote
 
-
-
-      while (puPosition < puEnd) {
-         if (*puPosition == uQuote) {
-            // Check for escaped quote (double quote)
-            if (puPosition + 1 < puEnd && *(puPosition + 1) == uQuote) {
-               puPosition += 2; // Skip both quotes
-            } else {
-               return puPosition + 1; // Return pointer past closing quote
+      while(puPosition < puEnd) 
+      {
+         if( *puPosition == uQuote ) 
+         {
+            if( uQuoteCount == 1 )
+            {
+               // Check for escaped quote (double quote)
+               if( puPosition + 1 < puEnd && *(puPosition + 1) == uQuote)
+               {
+                  puPosition += 2; // Skip both quotes
+               } 
+               else 
+               {
+                  return puPosition + 1; // Return pointer past closing quote
+               }
             }
-         } else {
+            else
+            {
+               // ## Match number of quotes text start with
+               auto uCount = uQuoteCount;
+               while( puPosition < puEnd && uCount > 0  && *puPosition == uQuote )
+               {
+                  ++puPosition;
+                  --uCount;
+               }
+
+               if( uCount == 0 ) return puPosition;                           // Return position after closing quote
+            }
+         } 
+         else 
+         {
             ++puPosition;
          }
       }
 
       return puPosition; // Unterminated quote
    }
+
+   const char* skip_quoted(const char* piPosition, const char* piEnd) const { return (const char*)skip_quoted(reinterpret_cast<const uint8_t*>( piPosition ), reinterpret_cast<const uint8_t*>( piEnd )); }
 
 
 
@@ -70,52 +95,64 @@ struct code
 };
 
 /** ---------------------------------------------------------------------------
- * @brief Finds the first occurrence of the byte string needle in the byte string pointed to by haystack.
- * @param pbszBegin start of text (haystack) to find string (needle) in 
+ * @brief Find a substring in a text block with optional scope handling
+ * @param pbszBegin start of text to search within
  * @param pbszEnd end of text
- * @param pbszFind pointer to string that is searched for within text
- * @param uLength length of string
- * @param csv parse rules, adapted to sql query format
- * @param puCharacterClass ascii text block (256 bytes) with character classes or null to use default
- * @return pointer to string in text if found, nullptr if not found
-*/
-const char* strstr( const char* pbszBegin, const char* pbszEnd, const char* pbszFind, unsigned uLength, const code& code )
+ * @param pbszFind substring to find
+ * @param uLength length of the substring to find
+ * @param code parsing rules for handling scopes and quotes
+ * @param bScope if true, considers scope characters for searching
+ * @return pointer to found substring or nullptr if not found
+ */
+const char* strstr( const char* pbszBegin, const char* pbszEnd, const char* pbszFind, unsigned uLength, const code& code, bool bScope )
 {                                                                                                  assert( pbszBegin <= pbszEnd );
    //if( puCharacterClass == nullptr ) puCharacterClass = pCharacterClass_s;
 
+
    const char* pbszPosition = pbszBegin;   // position in text
    char chFind = *pbszFind;                // first character in text to find
+   unsigned uScope = 1;                    // scope of search, how many characters to compare after first character has been found
+
+   if( bScope == true ) uScope = 0;                                            // if scope is true than we need to find scope character, otherwise we just compare first character
    
    pbszFind++;    // No need to compare first character
    uLength--;     // decrease length that is used when we compare rest after first character has been found
 
    while( pbszPosition < pbszEnd )
    {
+      if( bScope == true )
+      {
+         if( code.is_open_scope(*pbszPosition) == true ) uScope++;
+         else if( code.is_close_scope(*pbszPosition) == true )
+         {
+            if( uScope > 0 ) uScope--;
+         }
+      }
+
+      if( uScope == 0 )
+      {
+         pbszPosition++;
+         continue;
+      }
+      
       if( *pbszPosition != chFind )
       {
-         //if( !(puCharacterClass[*pbszPosition] & ASCII_TYPE_QUOTE) )
-         if( code.is )
+         if( code.is_quote( *pbszPosition ) == false )
          {
             pbszPosition++;
             continue;
          }
          else
          {
-            if( csv.is_quote( *pbszPosition ) == true )
-            {
-               // ## found quote, text within quote is skipped
-               pbszPosition = skip_quoted_g( pbszPosition );
-            }
-            else
-            {
-               pbszPosition++;
-               continue;
-            }
+            pbszPosition = code.skip_quoted(pbszPosition, pbszEnd );
          }
       }
       else
       {
-         if( uLength == 1 || memcmp( pbszPosition + 1, pbszFind, uLength ) == 0 ) return pbszPosition; // found text? return pointer to text
+         if( uLength == 1 || memcmp( pbszPosition + 1, pbszFind, uLength ) == 0 )
+         {
+            return pbszPosition;                                              // found text? return pointer to text
+         }
 
          pbszPosition++;
       }
@@ -129,4 +166,15 @@ const char* strstr( const char* pbszBegin, const char* pbszEnd, const char* pbsz
 
 TEST_CASE("[table] custom columns", "[strstr]") 
 {
+   std::string stringText = "This is a test string with key3 some patterns to find: [key1: `value1 key3`] [key2: \"value2 key3\"] [key3: value3]";
+   const char* begin = stringText.c_str();
+   const char* piEnd = begin + stringText.size();
+   const char* find = "key3";
+   unsigned uLength = (unsigned)strlen(find);
+   
+   const code codeParser('[', ']');
+   const char* result = strstr(begin, piEnd, find, uLength, codeParser, true);
+   
+   REQUIRE(result != nullptr);
+   REQUIRE(std::string(result, uLength) == find);
 }
