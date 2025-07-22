6e14c8fa75560f8b58747202ac1cbe55d5f5f7b4
Per Ghosh <per@gorep.se>
2025-07-22 17:21:35 +0200

    Refactor parsing functions and update tests
    - Simplified length calculation in `code::skip_quoted`.
    - Enhanced `code::read_value` to skip keys and find separators.
    - Updated test case in `PLAY_strstr.cpp` to include new key-value pairs and improved variable naming for clarity.

--- target/TOOLS/FileCleaner/playground/PLAY_strstr.cpp
+++ target/TOOLS/FileCleaner/playground/PLAY_strstr.cpp
@@ -21,17 +21,17 @@ TEST_CASE("[table] custom columns", "[strstr]")
 {
    using namespace gd::parse;
 
-   std::string stringText = "This is a test string with key3 some patterns to find: [key1: `value1 key3`] [key2: \"value2 key3\"] [key3: value3]";
+   std::string stringText = R"(This is a test string with key3 some patterns to find: [key1: `value1 key3`] [key2: \"value2 key33\"] [key3: value3] [key4: `````1234567890`````] )";
    const char* begin = stringText.c_str();
    const char* piEnd = begin + stringText.size();
-   const char* find = "key3";
-   unsigned uLength = (unsigned)strlen(find);
+   const char* piFindKey3 = "key3";
+   unsigned uLength = (unsigned)strlen(piFindKey3);
    
    const code codeParser('[', ']');
-   const char* piResult = strstr(begin, piEnd, find, uLength, codeParser, true);
+   const char* piResult = strstr(begin, piEnd, piFindKey3, uLength, codeParser, true);
    
    REQUIRE(piResult != nullptr);
-   REQUIRE(std::string(piResult, uLength) == find);
+   REQUIRE(std::string(piResult, uLength) == piFindKey3);
 
    if( piResult != nullptr )
    {
@@ -42,5 +42,33 @@ TEST_CASE("[table] custom columns", "[strstr]")
 
       auto result1_ = codeParser.read_value( std::string_view( piResult, piEnd ) );
       std::cout << "Read value: " << result1_ << std::endl;
+   }
+
+   const char* piFindKey2 = "key2";
+   unsigned uLength2 = (unsigned)strlen(piFindKey2);
+   const char* piResult2 = strstr(begin, piEnd, piFindKey2, uLength2, codeParser, true);
+
+   REQUIRE(piResult2 != nullptr);
+   REQUIRE(std::string(piResult2, uLength2) == piFindKey2);
+
+   if( piResult2 != nullptr )
+   {
+      piResult2 += uLength2;
+      auto result2_ = codeParser.read_value(piResult2, piEnd);
+      std::string_view stringValue2(result2_.first, result2_.second);
+      std::cout << "Found value for key2: " << stringValue2 << std::endl;
+   }
+
+   const char* piFindKey4 = "key4";
+   unsigned uLength4 = (unsigned)strlen(piFindKey4);
+   const char* piResult4 = strstr(begin, piEnd, piFindKey4, uLength4, codeParser, true);
+   REQUIRE(piResult4 != nullptr);
+   REQUIRE(std::string(piResult4, uLength4) == piFindKey4);
+
+   if( piResult4 != nullptr )
+   {
+      auto result4_ = codeParser.read_value(piResult4, piEnd);
+      std::string_view stringValue4(result4_.first, result4_.second);
+      std::cout << "Found value for key4: " << stringValue4 << std::endl;
    }
 }
