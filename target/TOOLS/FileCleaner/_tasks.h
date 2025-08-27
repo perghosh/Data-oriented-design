40e2c128f64262c03cbcc3a452c9684046c652a2
Per Ghosh <per@gorep.se>
2025-08-27 19:35:16 +0200

    docs

--- target/TOOLS/FileCleaner/_tasks.h
+++ target/TOOLS/FileCleaner/_tasks.h
@@ -10,3 +10,43 @@
 [description: "Prompt values are separated with ; if found the add those to options before reading values from arguments"]
 
 */
+
+
+/*
+@PROJECT [name: history]
+
+@TASK [title: add alias to history] [project:history] [assignee: per] [status:open] [created: 250828]
+[summary : Add alias to history]
+[description: "This task involves adding an alias to the history feature, allowing users to specify a shorthand command for accessing their command history."]
+
+@TASK [title: pin history item] [project:history] [assignee: per] [status:open] [created: 250828]
+[summary : Pin a history item]
+[description: "This task involves adding a pin feature to the history, allowing users to pin specific commands for easy access later."]   
+
+*/
+
+/*
+@PROJECT [name: print]
+
+@TASK [title: wrap the brief part] [project:print] [assignee: per] [status:open] [created: 250828]
+[summary : Wrap brief part]
+[description: "If max width is set then wrap the brief part to that width as the body is wrapped"]
+
+@TASK [title: check that csv print works] [project:print] [assignee: per] [status:open] [created: 250828]
+[summary : Check that CSV print works]
+[description: "This task involves verifying that the CSV print functionality is working as intended."]
+
+@TASK [title: frame] [project:print] [assignee: per] [status:open] [created: 250828]
+[summary : argument to set frame for output row]
+[description: "Frame is used if configured doing find searches with key value, with -frame or similar a frame should be printed even if no header or footer is set."]
+
+
+*/
+
+/*
+@PROJECT [name: help]
+
+@TASK [title: format command help] [project:help] [assignee: per] [status:open] [created: 250828]
+[summary : printing command help is not wrapped, that should be wraped as full help is wrapped]
+
+*/
