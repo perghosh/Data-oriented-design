b9f267f023077558538feffbc38ae470bc1e0840
Per Ghosh <per@gorep.se>
2025-11-02 14:07:50 +0100

    fix: version upped

--- target/TOOLS/FileCleaner/Application.cpp
+++ target/TOOLS/FileCleaner/Application.cpp
@@ -865,7 +865,7 @@ std::pair<bool, std::string> CApplication::Initialize( gd::cli::options& options
    }
    else if( stringCommandName == "version" )
    {
-      std::cout << "version 1.0.8" << "\n";
+      std::cout << "version 1.0.9" << "\n";
    }
    else
    {
