4426221e1414da8ebca3f987b13e122b7faf7815
Per Ghosh <per@gorep.se>
2025-11-15 00:07:29 +0100

    docs

--- target/TOOLS/FileCleaner/_tasks.h
+++ target/TOOLS/FileCleaner/_tasks.h
@@ -1,17 +1,17 @@
 /*
 Searches for tasks in the file and lists them here.
-cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "description;sample;idea" --header "project;title;id" --brief "summary" --footer "created;assignee;status"  --prompt "kv-where"
+cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "description;sample;idea" --header "project;title;id" --brief "summary" --footer "created;user;status"  --prompt "kv-where"
 
 */
 
 /*
 @PROJECT [name: hot] [description: Hot operations, collec what to do here]
 
-@TASK [title: prompt] [project:hot] [assignee: per] [status:open] [level: 2] [created: 250830]
+@TASK [title: prompt] [project:hot] [user: per] [status:open] [level: 2] [created: 250830]
 [summary: running prompt values from history do not ask for value because it is done later compared to original]
 [description: "Move logick for prompt to enable set prompt value from history"]
 
-@TASK [title: prompt] [project:hot] [assignee: per] [status:open] [level: 1] [created: 250830]
+@TASK [title: prompt] [project:hot] [user: per] [status:open] [level: 1] [created: 250830]
 [summary: better description for prompt values]
 [description: "Add better description for prompt values, what it does and how to use it"]
 */
@@ -20,11 +20,11 @@ cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "
 /*
 @PROJECT [name: prompt]
 
-@TASK [title: add prompt option to options class] [project:prompt] [assignee: per] [status:open] [created: 250828]
+@TASK [title: add prompt option to options class] [project:prompt] [user: per] [status:open] [created: 250828]
 [summary : Add prompt option to options class]
 [description: "This task involves adding a prompt option to the options class, allowing users to specify whether they want to be prompted for input before executing a command."]
 
-@TASK [title: add prompt values] [project:prompt] [assignee: per] [status:open] [created: 250828]
+@TASK [title: add prompt values] [project:prompt] [user: per] [status:open] [created: 250828]
 [summary : Add values specified in prompt]
 [description: "Prompt values are separated with ; if found the add those to options before reading values from arguments"]
 
@@ -35,12 +35,20 @@ cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "
 /*
 @PROJECT [name: history] [description: History operations]
 
-@TASK [title: home directory] [project:history] [assignee: kevin] [status:closed] [created: 250830]
+@TASK [title: home directory] [project:history] [user: kevin] [status:closed] [created: 250830]
 [summary : Replace GetHistoryPath_s with CApplication::FolderGetHome_s to get home directory]
 
-@TASK [title: pin history item] [project:history] [assignee: per] [status:open] [created: 250828]
+@TASK [title: pin history item] [project:history] [user: per] [status:open] [created: 250828]
 [summary : Pin a history item]
 [description: "This task involves adding a pin feature to the history, allowing users to pin specific commands for easy access later."]   
+
+@TASK [title: unpin history item] [project:history] [user: per] [status:open] [created: 250828]
+[summary : Unpin a history item]
+[description: "This task involves adding an unpin feature to the history, allowing users to unpin specific commands that are no longer needed."]
+
+@TASK [title: check for duplicate aliases] [project:history] [user: per] [status:open] [created: 251111]
+[summary : Check for duplicate aliases]
+[description: "This task involves checking for duplicate aliases in the history. If there are any print some sort of warning"]
 
 */
 
@@ -49,15 +57,15 @@ cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "
 /*
 @PROJECT [name: print]
 
-@TASK [title: check that csv print works] [project:print] [assignee: per] [status:open] [created: 250828]
+@TASK [title: check that csv print works] [project:print] [user: per] [status:open] [created: 250828]
 [summary: Check that CSV print works]
 [description: "This task involves verifying that the CSV print functionality is working as intended."]
 
-@TASK [title: border/frame] [project:print] [assignee: per] [status:open] [created: 250828]
+@TASK [title: border/frame] [project:print] [user: per] [status:open] [created: 250828]
 [summary: argument to set border/frame for output row]
 [description: "Frame is used if configured doing find searches with key value, with -frame or similar a frame should be printed even if no header or footer is set."]
 
-@TASK [title: format template] [project:print] [assignee: per] [status:open] [level:4] [created: 250830]
+@TASK [title: format template] [project:print] [user: per] [status:open] [level:4] [created: 250830]
 [summary: template for printing]
 [description: "To get better way to format output based on what type of search thats done figure out some sort of template to read information from. Similar to jinja maybe"]
 
@@ -68,10 +76,10 @@ cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "
 /*
 @PROJECT [name: help]
 
-@TASK [summary: add global options to command help] [project:help] [assignee: per] [status:closed] [created: 251111]
+@TASK [summary: add global options to command help] [project:help] [user: per] [status:closed] [created: 251111]
 [summary : print help for specific command, then global options should be printed also]
 
-@TASK [summary: print only global options] [project:help] [assignee: per] [status:closed] [created: 251111]
+@TASK [summary: print only global options] [project:help] [user: per] [status:closed] [created: 251111]
 [summary : add flag for only print global options]
 */
 
@@ -79,5 +87,35 @@ cleaner find -filter "*.h;*.cpp" -R --pattern "@TASK" --segment comment --keys "
 
 /*
 @PROJECT [name: serialize-table] [summary: Serialize and deserialize table data]
+
+*/
+
+// ----------------------------------------------------------------------------
+
+/*
+@PROJECT [name: many-command-lines] [summary: Pass many command lines, in file or clipboard]
+
+@TASK [summary: add subcommand] [project:many-command-lines] [user: per] [status:open] [created: 251111] [type: code]
+[description: add subbcommand to cleaner that take a file or checks for clipboard where it should be able to process multiple lines and execute them]
+
+@TASK [summary: help for subcommand] [project:many-command-lines] [user: per] [status:open] [created: 251111] [type: code]
+[description: fix help for subcommand]
+
+@TASK [summary: write tests for cleaner] [project:many-command-lines] [user: per] [status:open] [created: 251111] [type: code]
+[description: this many command line should be used to test cleaner for proper functionality]
+
+@TASK [summary: docs for many-command-lines] [project:many-command-lines] [user: per] [status:open] [created: 251111] [type: docs]
+[description: add documentation for many-command-lines logic]
+
+@TASK [summary: test many-command-lines] [project:many-command-lines] [user: per] [status:open] [created: 251111] [type: test]
+[description: test many-command-lines so it works]
+
+*/
+
+/*
+@PROJECT [name: folder operations] [summary: Perform operations on folders]
+
+@TASK [summary: sum count based on folder] [project:folder operations] [user: per] [status:open] [created: 251112] [type: code]
+[description: count information in files is normally presented for each file, summing up the total count for the folder is also needed]
 
 */
