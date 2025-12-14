9fe071c66d5badcefcfa7fbbcb9ca22a2b639fb9
Per <per@gorep.se>
2025-12-14 21:28:24 +0100

    chore

--- target/server/http/playground/PLAY_uri.cpp
+++ target/server/http/playground/PLAY_uri.cpp
@@ -17,14 +17,53 @@
 
 #include "catch2/catch_amalgamated.hpp"
 
+TEST_CASE( "[uri] kevin lek", "[uri]" )
+{
+    gd::argument::arguments argumentsUri;
+
+    std::string stringUri = "http://localhost:8080/one/two/three/four/five?arg=15.5&key=value&arg1=10#fragment";
+    std::string stringUri2 = "http://localhost:8080/one/two/three/four/five?arg=15.5&key=value&arg1=10#fragment";
+    std::cout << "\n\n## " << stringUri << "\n" << std::endl;
+
+    auto [succes, error] = gd::parse::uri::parse( stringUri, argumentsUri );                       REQUIRE( succes == true );
+    std::string stringUriResult = gd::argument::debug::print(argumentsUri);
+    std::cout << stringUriResult << std::endl;
+
+    std::string stringPath = argumentsUri["path"].as_string();
+    std::string stringQuery = argumentsUri["query"].as_string();
+
+    std::cout << stringPath << " " << stringQuery << "\n";
+
+    gd::argument::arguments argumentsQuery;
+    gd::parse::uri::parse_query(stringQuery, argumentsQuery);
+
+    std::string stringQueryResult = gd::argument::debug::print(argumentsQuery);
+    std::cout << stringQueryResult << std::endl;
+
+    double dNumber = argumentsQuery[ "arg" ].as_double();
+    std::string stringKey = argumentsQuery[ "key" ].as_string();
+    int iNumber = argumentsQuery[ "arg1" ].as_int();
+
+    std::cout << dNumber << " " << stringKey << " " << iNumber << std::endl;
+
+    for( auto it = argumentsQuery.named_begin(); it != argumentsQuery.named_end(); it++)
+    {
+       std::string stringKey( it->first );
+       std::string stringValue = it->second.as_string();
+
+       std::cout << "Key = " << stringKey << " Value = " << stringValue << std::endl;
+    }
+}
 
 TEST_CASE("[uri] test uri logic", "[uri]") 
 {
+    return;
    std::cout << "test uri logic" << std::endl;
 
    // test with gd::argument::arguments
 
    {
+       
       gd::argument::arguments argumentsUri;
       std::string uri = "http://username:password@hostname:9090/path?arg=value#anchor";
       std::cout << "\n\n## " << uri << "\n" << std::endl;
