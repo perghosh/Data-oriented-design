db535b317e7f4e5fc6383fabf99a4378d7c0bd67
Per Ghosh <per@gorep.se>
2026-01-01 20:23:08 +0100

    docs

---
+++ misc/html/_ai_.js
@@ -0,0 +1,14 @@
+/*
+javascript style guide
+let iNumber = 10;
+const dDecimal = 3.14;
+const sString = "Hello, World!";
+var bBoolean = true;
+function CalculateSum(a, b) { ... }
+const oPerson = { FirstName: "John", LastName: "Doe" };
+let aNumbers = [1, 2, 3, 4, 5];
+let eElement = document.getElementById("myElement");
+
+// do not use abbreviations in variable names, this is wrong: let iNum = 10;
+// only loop variable can be abbreviated like: for (let i = 0; i < 10; i++) { ... }
+*/
