9aefbdca6d8c928a809a1fa6ea9a38bc8f5e5a8f
Per Ghosh <per@gorep.se>
2026-01-07 14:32:50 +0100

    feat: table

--- target/WEB/admin/http/js/classes/gd_data_table.js
+++ target/WEB/admin/http/js/classes/gd_data_table.js
@@ -1,83 +1,206 @@
-class Table
-{
-    /**
-     * Constructor
-     * @param {Object} options_
-     * @param {Array<Array>} [options_.aTable=[]]
-     * @param {Array<string>} [options_.aColumn=[]]
-     */
-    constructor(options_ = {})
-    {
-        const oOptions = Object.assign({
-            aTable: [],
-            aColumn: []
-        }, options_);
+class Table {
 
-        this.aTable = oOptions.aTable;
-        this.aColumn = oOptions.aColumn;
+
+   static column = class {
+      /**
+       * @param {Object|string} options_ - Either a configuration object or the column name string
+       * @param {string} options_.sName
+       * @param {string} [options_.sAlias]
+       * @param {string} [options_.sType="unknown"]
+       * @param {number} [options_.iState=0]
+       * @param {number} [options_.iSpecificType=0]
+       */
+      constructor(options_ = {}) {
+         if( typeof options_ === "string" ) { options_ = {sName: options_}; }
+         if( typeof options_ !== "object" ) { throw new Error("Invalid argument"); }
+
+         const oOptions = Object.assign({sName: "",  sAlias: "",  sType: "string", iState: 0, iSpecificType: 0 }, options_);
+
+         this.sName = oOptions.sName || "";
+         this.sAlias = oOptions.sAlias || this.sName;
+         this.sType = oOptions.sType || "unknown";
+         this.iState = oOptions.iState || 0; // e.g., 0: none, 1: sorted asc, 2: sorted desc
+         this.iSpecificType = oOptions.iSpecificType || 0;
+      }
+
+      is_string() { return this.sType === "string"; }
+      is_number() { return this.sType === "number"; }
+   }
+
+   /** -----------------------------------------------------------------------
+    * Constructor
+    * @param {Object} options_
+    * @param {Array<Array>} [options_.aTable=[]]
+    * @param {Array<string>} [options_.aColumn=[]]
+    */
+   constructor(columns_ = [], options_ = {}) {
+      if( typeof columns_ === "string" ) {
+         columns_ = columns_.split(",")
+      }
+      if( !Array.isArray(columns_) ) { throw new Error("Invalid argument"); }
+
+      const aColumn = Array.isArray( columns_ ) ? columns_.map(column => new Table.column(column)) : [];
+
+      this.aTable = options_.aTable || [];
+      this.aColumn = options_.aColumn || aColumn;
+   }
+
+   /** -----------------------------------------------------------------------
+    * Get column index by name or alias
+    * @param {string|Object} column_ if string then match name or alias, if object then pick type and match
+    * @returns {number} index to column or -1 if not found
+    */
+   GetColumnIndex( column_ ) {
+      if( typeof column_ === "string") {
+         // ## First pass: check by name .....................................
+         for(let i = 0; i < this.aColumn.length; i++) {
+            if(this.aColumn[i].sName === column_) { return i; }
+         }
+
+         // ## Second pass: check by alias if name not found .................
+         for(let i = 0; i < this.aColumn.length; i++) {
+            if(this.aColumn[i].sAlias === column_) { return i; }
+         }
+         return -1;
+      }
+      if( typeof column_ === "object") {
+         // ## If sName .....................................
+         if( column_.sName ) {
+            for(let i = 0; i < this.aColumn.length; i++) { if(this.aColumn[i].sName === column_.sName) { return i; } }
+         }
+         // ## If sAlias .....................................
+         if( column_.sAlias ) {
+            for(let i = 0; i < this.aColumn.length; i++) { if(this.aColumn[i].sAlias === column_.sAlias) { return i; } }
+         }
+
+         return -1;
+      }
+
+      return column_;
+   }
+
+   /** -----------------------------------------------------------------------
+    * Get header row use alias or name in columns and generates a row that is returned
+    */
+    GetHeaderRow() {
+       return this.aColumn.map(column => column.sAlias || column.sName);
     }
 
-    /**
-     * Get table data
-     * @param {Object} options_
-     * @param {boolean} bColumn 
-     * @param {number} iSort
-     */
-    GetData( options_ )
-    {
-        const oOptions = Object.assign({
-            bColumn: true,
-            iSort: 0
-        }, options_);
 
-        let aData = [];
+   /** -----------------------------------------------------------------------
+    * Get table data
+    * @param {Object} options_
+    * @param {boolean} bColumn
+    * @param {number} iSort that is one based column index, not zero based
+    */
+   GetData(options_) {
+      const oOptions = Object.assign({
+         bHeader: true,
+         iSort: 0
+      }, options_);
 
-        if(oOptions.bColumn == true) { aData.push(...this.aColumn) }
-        aData.push(...this.aTable);
+      let aData = [];
 
-        return aData;
-    }
+      for(let i = 0; i < this.aTable.length; i++) { aData.push(this.GetRow(i)); }
 
-    /**
-     * Add rows to the table
-     * @param {Array | string} table_ - Data to add (string, row array, or array of rows)
-     * @param {string} sSeperator - Optional separator for string input (default: ",")
-     */
-    Add(table_, sSeperator)
-    {
-        let aTable = table_;
-        if(typeof table_ === "string")
-        {
-            if(!sSeperator) {sSeperator = ","}
-            aTable = [table_.split(sSeperator)];   
-        }
-        else if(Array.isArray(table_) && table_.every(Array.isArray) == false)
-        {
-            aTable = [table_];
-        }
+      if(oOptions.iSort > 0) {
+         const iSort = oOptions.iSort - 1;
+         aData.sort((a, b) => a[iSort] - b[iSort]);
+      }
 
-        for(let i = 0; i < aTable.length; i++) // undefined
-        {
-            this.aTable.push(aTable[i]);
-        }
-        console.log(this.aTable);
-    }
-    
-    /**
-     * Deletes one or more rows from the table.
-     *
-     * @param {number} iPosition - The starting index from which rows will be deleted
-     * @param {number} iLength   - Number of rows to delete (defaults to 1)
-     */
-    Delete(iPosition, iLength)
-    {
-        const iRows = this.aTable.length;
-        const iMaxLength = iRows - iPosition;
+      if(oOptions.iSort < 0) {
+         const iSort = Math.abs(oOptions.iSort) - 1;
+         aData.sort((a, b) => b[iSort] - a[iSort]);
+      }
 
-        if( !iLength || iLength === 0 ) { iLength = 1; }  
+      if( oOptions.bHeader) { aData.unshift(this.GetHeaderRow()); }
 
-        iLength = Math.min(iLength, iMaxLength);
+      return aData;
+   }
 
-        this.aTable.splice(iPosition, iLength);
-    }
+   /** -----------------------------------------------------------------------
+    * Get cell value
+    * @param {number} iRow index for row
+    * @param {number | string} column_ index for column or column name
+    */
+   GetCellValue(iRow, column_) {
+      let iColumn = column_;
+      if( typeof column_ === "string") { iColumn = this.GetColumnIndex(column_); }
+      if(iRow < 0 || iRow >= this.aTable.length || iColumn < 0 || iColumn >= this.aColumn.length) {
+         return null;
+      }
+
+      return this._GetCellValue(iRow, iColumn);
+   }
+
+   _GetCellValue(iRow, iColumn) {
+      let value_ = this.aTable[iRow][iColumn];
+
+      if(Array.isArray(value_)) { value_ = value_[0];  }                      // if column is array, return first element
+
+      return value_;
+   }
+
+   /** -----------------------------------------------------------------------
+    * Get row data
+    * @param {number} iRow index for row
+    */
+    GetRow(iRow) {
+      if(iRow < 0 || iRow >= this.aTable.length) { return null; }
+
+      return this._GetRow(iRow);
+   }
+
+   _GetRow(iRow) {
+     const row_ = [];
+     for(let iColumn = 0; iColumn < this.aColumn.length; iColumn++) {
+        row_.push(this._GetCellValue(iRow, iColumn));
+     }
+
+     return row_;
+  }
+
+
+   // Check if table is empty ------------------------------------------------
+   Empty() { return this.aTable.length === 0; }
+
+   // Return number of rows --------------------------------------------------
+   Size() {  return this.aTable.length; }
+
+   /** -----------------------------------------------------------------------
+    * Add rows to the table
+    * @param {Array | string} table_ - Data to add (string, row array, or array of rows)
+    * @param {string} sSeperator - Optional separator for string input (default: ",")
+    */
+   Add(table_, sSeperator) {
+      let aTable = table_;
+      if(typeof table_ === "string") {
+         if(!sSeperator) { sSeperator = "," }                                 // default separator is ","
+         aTable = [table_.split(sSeperator)];
+      }
+      else if(Array.isArray(table_) && table_.every(Array.isArray) == false) { aTable = [table_]; } // check for single [] to add
+
+      for(let i = 0; i < aTable.length; i++) // undefined
+      {
+         this.aTable.push(aTable[i]);
+      }
+      console.log(this.aTable);
+   }
+
+   /** -----------------------------------------------------------------------
+    * Deletes one or more rows from the table.
+    *
+    * @param {number} iPosition - The starting index from which rows will be deleted
+    * @param {number} iLength   - Number of rows to delete (defaults to 1)
+    */
+   Delete(iPosition, iLength) {
+      const iRows = this.aTable.length;
+      const iMaxLength = iRows - iPosition;
+
+      if(!iLength || iLength === 0) { iLength = 1; }
+
+      iLength = Math.min(iLength, iMaxLength);
+
+      this.aTable.splice(iPosition, iLength);
+   }
 }
