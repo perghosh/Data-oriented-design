736c0ab27a5eb96f9c9076da55788f1251cda2f2
Per Ghosh <per@gorep.se>
2026-01-29 08:34:24 +0100

    fix

---
+++ target/server/http/playground/PLAY_Endpoint.cpp
@@ -0,0 +1,30 @@
+// @FILE [tag: strstr, playground] [description: Key value and finding things in string] [type: playground]
+
+#include <filesystem>
+#ifdef _WIN32
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <windows.h>
+#include <psapi.h>
+#endif
+
+#include "gd/gd_arguments.h"
+#include "gd/gd_arguments_shared.h"
+#include "gd/gd_table_io.h"
+
+#include "../Session.h"
+#include "../Document.h"
+#include "../Application.h"
+
+#include "main.h"
+
+#include "catch2/catch_amalgamated.hpp"
+
+
+TEST_CASE("[session] test uri logic", "[session]") 
+{
+   std::unique_ptr< CApplication > papplication_ = std::make_unique<CApplication>();
+   papplication_g = papplication_.get();
+
+}
