b136f1f6a27640e73ea488fa53931a1f201e2b68
Per Ghosh <per@gorep.se>
2026-02-09 13:25:02 +0100

    Expand SQL dialects and add tag dispatching
    Reorganize enumSqlDialect into compatibility groups
    Add tag_not_null for table value lookup prioritizing fixed
    columns
    Add type support to CRENDERSql fields

--- target/server/http/render/RENDERSql.cpp
+++ target/server/http/render/RENDERSql.cpp
@@ -27,6 +27,7 @@ void CRENDERSql::Initialize()
       m_pcolumnsField_s->add( "string", uSize, "column" );                    // name for column in table
       m_pcolumnsField_s->add( "string", uSize, "alias" );                     // alias for column in table
       m_pcolumnsField_s->add( "string", uSize * 2, "value" );                 // value for column in table
+      m_pcolumnsField_s->add( "uint32", 0, "type" );                          // type of value
       m_pcolumnsField_s->add_reference();
    }
 
@@ -42,7 +43,11 @@ void CRENDERSql::AddValue( const gd::argument::arguments argumentsField )
    for( auto [key_, value_] : argumentsField.named() )
    {
       if( value_.is_string() == true ) { Add( key_, value_.as_string_view() ); }
-      else                             { Add( key_, value_ ); }
+      else
+      { 
+         if( key_ == "type" ) { m_tableField.cell_set( uRow, eColumnFieldType, value_); }
+         else { Add( key_, value_ ); }
+      }
    }
 }
 
@@ -76,6 +81,39 @@ std::pair<bool,std::string> CRENDERSql::GetQuery( enumSqlQueryType eSqlQueryType
          return {false, "Invalid query type"};
    }
    return {true, stringQuery};
+}
+
+std::pair<bool,std::string> CRENDERSql::ToSqlInsert( std::string& stringQuery )
+{
+   std::string stringTable = m_tableField.cell_get_variant_view(0u, "table", gd::table::tag_not_null{}).as_string();
+
+   gd::sql::query queryInsert( gd::sql::eSqlInsert, stringTable, gd::sql::tag_table{});
+   queryInsert.sql_set_dialect( gd::sql::sql_get_dialect_g("sqlite"));
+
+   std::vector<gd::variant_view> vectorValue;
+   for( auto itRow = m_tableField.row_begin(); itRow != m_tableField.row_end(); ++itRow )
+   {
+      std::string stringColumn = itRow.cell_get_variant_view( "column", gd::table::tag_not_null{}).as_string();
+      queryInsert.field_add( stringColumn );
+      auto value_ = itRow.cell_get_variant_view("value", gd::table::tag_not_null{});
+      vectorValue.push_back( value_ );
+   }
+
+   // ## Generate insert query ..............................................
+
+   std::string stringInsertSql;
+	stringInsertSql += "INSERT INTO ";
+	stringInsertSql += queryInsert.sql_get_insert();
+	stringInsertSql += "\nVALUES(";
+	stringInsertSql += gd::sql::query::values_get_s( vectorValue ).second;// append values from name value pairs
+	stringInsertSql += ")";
+
+   // ## Set the out string to query .........................................
+
+   if( stringQuery.empty() == true ) stringQuery = std::move( stringInsertSql );
+   else stringQuery += "\n\n" + stringInsertSql;
+
+   return { true, "" };
 }
 
 
