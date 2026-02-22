ea67ea1caf6d553ba266e6d496e277fd23cd8b23
Per Ghosh <per@gorep.se>
2026-02-22 18:28:13 +0100

    Enhance field_builder API and documentation
    Expanded field_builder documentation with usage examples. Added lvalue and rvalue overloads for all modifier methods to improve flexibility. Introduced implicit conversion operators to arguments types and a get_parttype() getter. Improved documentation for the stream operator<<. Fixed a test to use the correct field_add_parttype method.

--- external/gd/gd_sql_query_builder.h
+++ external/gd/gd_sql_query_builder.h
@@ -33,10 +33,17 @@
 
 _GD_SQL_QUERY_BEGIN
 
-/** =======================================================================
+/** ==========================================================================
  * @brief Fluent builder for field definitions in SQL queries.
  * 
- * A proxy object that allows chaining of method calls to set field attributes and specify the SQL query part it belongs to.
+ * A proxy object that allows chaining of method calls to set field attributes 
+ * and specify the SQL query part it belongs to.
+ * 
+ * @par Example
+ * @code
+ * query << field_g("name").as("alias").orderby();
+ * query << field_g("id").select().type("INTEGER");
+ * @endcode
  */
 struct field_builder
 {
@@ -48,24 +55,48 @@ struct field_builder
 
     // @API [tag: operator] [summary: simplify with operators for use in methods]
     
+     /// Implicit conversion to arguments reference for passing to query methods
     operator gd::argument::arguments&() { return m_arguments; }
+     /// Implicit conversion to arguments reference for passing to query methods
     operator const gd::argument::arguments&() const { return m_arguments; }
+    
+    // @API [tag: getter, setter] [summary: get and set members]
+
+    unsigned get_parttype() const noexcept { return m_uPartType; }
     
     // @API [tag: attribute] [summary: set attribute values for field]
 
-    field_builder&& as(std::string_view v_) &&           { m_arguments.set("alias", v_);   return std::move(*this); }
-    field_builder&& value(gd::variant_view v_) &&        { m_arguments.set("value", v_);   return std::move(*this); }
-    field_builder&& type(gd::variant_view v_) &&         { m_arguments.set("type", v_);    return std::move(*this); }
-    field_builder&& raw(std::string_view v_) &&          { m_arguments.set("raw", v_);     return std::move(*this); }
+    field_builder& as(std::string_view v_) &       { m_arguments.set("alias", v_);   return *this; }
+    field_builder&& as(std::string_view v_) &&     { m_arguments.set("alias", v_);   return std::move(*this); }
+    
+    field_builder& value(gd::variant_view v_) &    { m_arguments.set("value", v_);   return *this; }
+    field_builder&& value(gd::variant_view v_) &&  { m_arguments.set("value", v_);   return std::move(*this); }
+    
+    field_builder& type(gd::variant_view v_) &     { m_arguments.set("type", v_);    return *this; }
+    field_builder&& type(gd::variant_view v_) &&   { m_arguments.set("type", v_);    return std::move(*this); }
+    
+    field_builder& raw(std::string_view v_) &      { m_arguments.set("raw", v_);     return *this; }
+    field_builder&& raw(std::string_view v_) &&    { m_arguments.set("raw", v_);     return std::move(*this); }
 
     // @API [tag: sql, type] [summary: Part-type shortcuts]
 
-    field_builder&& select() &&    { m_uPartType = eSqlPartSelect;    return std::move(*this); }
-    field_builder&& orderby() &&   { m_uPartType = eSqlPartOrderBy;   return std::move(*this); }
-    field_builder&& groupby() &&   { m_uPartType = eSqlPartGroupBy;   return std::move(*this); }
-    field_builder&& insert() &&    { m_uPartType = eSqlPartInsert;    return std::move(*this); }
-    field_builder&& update() &&    { m_uPartType = eSqlPartUpdate;    return std::move(*this); }
-    field_builder&& returning() && { m_uPartType = eSqlPartReturning; return std::move(*this); }
+    field_builder& select() &       { m_uPartType = eSqlPartSelect;    return *this; }
+    field_builder&& select() &&     { m_uPartType = eSqlPartSelect;    return std::move(*this); }
+    
+    field_builder& orderby() &      { m_uPartType = eSqlPartOrderBy;   return *this; }
+    field_builder&& orderby() &&    { m_uPartType = eSqlPartOrderBy;   return std::move(*this); }
+
+    field_builder& groupby() &      { m_uPartType = eSqlPartGroupBy;   return *this; }
+    field_builder&& groupby() &&    { m_uPartType = eSqlPartGroupBy;   return std::move(*this); }
+
+    field_builder&  insert() &      { m_uPartType = eSqlPartInsert;    return *this; }
+    field_builder&& insert() &&     { m_uPartType = eSqlPartInsert;    return std::move(*this); }
+
+    field_builder& update() &       { m_uPartType = eSqlPartUpdate;    return *this; }
+    field_builder&& update() &&     { m_uPartType = eSqlPartUpdate;    return std::move(*this); }
+
+    field_builder& returning() &    { m_uPartType = eSqlPartReturning; return *this; }
+    field_builder&& returning() &&  { m_uPartType = eSqlPartReturning; return std::move(*this); }
     
     unsigned m_uPartType = 0;            ///< part type in sql query
     gd::argument::arguments m_arguments; ///< arguments used to pass into query
@@ -84,7 +115,16 @@ inline field_builder field_g(std::string_view stringName, void* pBuffer_, std::s
 { return field_builder{stringName, std::span<std::byte>{(std::byte*)pBuffer_, uSize}}; }
 
 
-/// global operator to add field builder to query: query << field_g("name").as("alias").orderby();
+/// ---------------------------------------------------------------------------
+/// @brief Stream operator to add a field builder to a query
+/// @param query_ The query to add the field to
+/// @param fieldbuilder_ The field builder (moved from)
+/// @return Reference to the query for chaining
+/// @par Example
+/// @code
+/// query << field_g("id").as("user_id").select() 
+///       << field_g("name").orderby();
+/// @endcode
 inline query& operator<<(query& query_, field_builder&& fieldbuilder_)
 {
    if(fieldbuilder_.m_uPartType != 0)
